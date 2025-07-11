cc9571ee03715168119f86776cbb2e183c7eeb73
MateusAciole <devmateusabf@gmail.com>
2025-07-11 15:44:27 -0300

    Fix: Altera o nome das rotas do controller dos ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,11 +1,9 @@
 package dev.java10x.CadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasVindas")
@@ -13,5 +11,33 @@ public class NinjaController {
         return "Bem vindo!";
 
     }
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(){
+        return "Ninjas listados";
+    }
+
+    @GetMapping("/listarID")
+    public String listarNinjaId(){
+        return "Ninja por ID listado";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarNinjaId(){
+        return "Ninja alterado por ID";
+    }
+
+    @DeleteMapping("/deletarId")
+    public String deletarNinjaId(){
+        return "Ninja deletado por ID";
+    }
+
+
+
 
 }
